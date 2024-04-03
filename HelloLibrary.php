90c38c39b6010f5c539b4796db0702f8cf9d4240
Agung Timotius Sinaga <agungsinaga2007@gmail.com>
2024-04-03 13:12:31 +0700

    remove file HelloLibrary.php

--- HelloLibrary.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/vendor/autoload.php';
-
-$customer = new \Programmerzamannow\Belajar\Customer("Agung");
-
-echo $customer->sayHello() . PHP_EOL;
-echo $customer->sayHello("Vior") . PHP_EOL;
